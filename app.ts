f70fddc1b98f73cf1e6cdcd0edc501e8fd66e75c
PawelYarko <pawelyarko@gmail.com>
2022-09-13 22:44:22 +0200

    Update app.ts

--- app.ts
+++ app.ts
@@ -2,6 +2,13 @@ const button = document.querySelector("button");
 const input1 = document.getElementById("num1")! as HTMLInputElement;
 const input2 = document.getElementById("num2")! as HTMLInputElement;
 
+let age: number = 50;
+let name = 'Max';
+let toggle: boolean = true;
+let empty:null = null;
+let notInitialize:undefined = undefined;
+let callback = (a) => { return 100 + a };
+
 function add (num1:number, num2:number){
     return num1 + num2;
 }
