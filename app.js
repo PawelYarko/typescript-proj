7f150ae07fb71cea043d109fb4f6944d53f82ed2
PawelYarko <pawelyarko@gmail.com>
2022-09-17 21:26:31 +0200

    module 2 part 1 types

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var button = document.querySelector("button");
 var input1 = document.getElementById("num1");
 var input2 = document.getElementById("num2");
+var age = 50;
+var name = 'Max';
+var toggle = true;
+var empty = null;
+var notInitialize = undefined;
+var callback = function (a) { return 100 + a; };
 function add(num1, num2) {
     return num1 + num2;
 }
--- app.ts
+++ app.ts
@@ -2,12 +2,18 @@ const button = document.querySelector("button");
 const input1 = document.getElementById("num1")! as HTMLInputElement;
 const input2 = document.getElementById("num2")! as HTMLInputElement;
 
-let age: number = 50;
-let name = 'Max';
-let toggle: boolean = true;
-let empty:null = null;
-let notInitialize:undefined = undefined;
-let callback = (a) => { return 100 + a };
+let age:number;
+age = 50;
+let name:string;
+name = 'Max';
+let toggle:boolean;
+toggle = true;
+let empty:null;
+empty = null;
+let notInitialize:undefined;
+notInitialize = undefined;
+let callback = (a: number) => number;
+callback = (a) => { return 100 + a };
 
 function add (num1:number, num2:number){
     return num1 + num2;
@@ -16,3 +22,12 @@ function add (num1:number, num2:number){
 button.addEventListener("click", function(){
     console.log(add(+input1.value, +input2.value));
 })
+
+let anything:any;
+anything = -20;
+anything = 'Text';
+anything = {};
+
+let some:unknown;some = 'Text';
+let str: string;
+if (typeof some === 'string') {  str = some;}
